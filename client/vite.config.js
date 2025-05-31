d1bae410a6f32dd1828d0e704e0df962372e7d03
nitin17-01 <nitinmaurya0704@gmail.com>
2025-05-31 23:27:39 +0530

    added dist folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
-  server : {
-    proxy : {
-      "/api" : {
-        target : "http://localhost:4000"
-      }
-    }
-  }
-})
+  plugins: [react()],
+  build: {
+    outDir: 'dist' // 
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fragrancefinder-gphq.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
